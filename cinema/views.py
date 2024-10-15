d64ebadd46e0dc8750f0dd38591fb9f85f72c4a4
Olga-Gynguliak <olga.gynguliak@gmail.com>
2024-10-15 18:40:15 +0300

    "Solution py-movie-api_1"

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import status
+from django.shortcuts import get_object_or_404
 
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
@@ -15,32 +16,24 @@ def movie_list(request):
 
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
 
 @api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
-    try:
-        movies = Movie.objects.get(pk=pk)
-    except Movie.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    movie = get_object_or_404(Movie, pk=pk)
 
     if request.method == "GET":
-        serializer = MovieSerializer(movies)
-        return Response(serializer.data)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == "PUT":
-        serializer = MovieSerializer(movies, data=request.data)
-        if serializer.is_valid():
-            serializer.save
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = MovieSerializer(movie, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == "DELETE":
-        movies.delete()
+        movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
